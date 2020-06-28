f422de29072e0aca5ace8855ae6995937dba74be
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-28 11:54:56 +1000

    feat(fonts): add document fonts

---
+++ lib/fonts.ts
@@ -0,0 +1,12 @@
+import path from "path"
+
+const fonts = {
+  Roboto: {
+    normal: path.resolve(__dirname + '/fonts/Roboto-Regular.ttf'),
+    bold: path.resolve(__dirname + '/fonts/Roboto-Medium.ttf'),
+    italics: path.resolve(__dirname + '/fonts/Roboto-Italic.ttf'),
+    bolditalics: path.resolve(__dirname + '/fonts/Roboto-MediumItalic.ttf')
+  }
+}
+
+export default fonts
